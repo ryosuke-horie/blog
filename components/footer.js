9ebbd8dfe1939d560b837b20e336e68164d9bf8d
ryosuke-horie <ryosuke03140@gmial.com>
2023-05-03 16:50:28 +0900

    LogoとNavを追加

--- components/footer.js
+++ components/footer.js
@@ -1,3 +1,9 @@
+import Logo from "components/logo"
+
 export default function Footer() {
-    return <footer>FOOTER</footer>
+    return (
+        <footer>
+            <Logo />
+        </footer>
+    )
 }
--- components/header.js
+++ components/header.js
@@ -1,3 +1,11 @@
+import Logo from "components/logo"
+import Nav from "components/nav"
+
 export default function Header() {
-    return <header>HEADER</header>
+    return (
+    <header>
+        <Logo />
+        <Nav />
+    </header>
+    )
 }
