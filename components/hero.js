08450b6ea8ef4572fde8259a5cf54d1d87c185e8
ryosuke-horie <ryosuke03140@gmial.com>
2023-05-03 18:22:10 +0900

    共通スタイルを利用してheroのレイアウトを調整

--- components/hero.js
+++ components/hero.js
@@ -2,9 +2,11 @@ import styles from "styles/hero.module.css"
 
 export default function Hero({ title, subtitle, imageOn = false }) {
     return (
-        <div className={styles.text}>
-            <h1 className={styles.title}>{title}</h1>
-            <p className={styles.subtitle}>{subtitle}</p>
+        <div className={styles.flexContainer}>
+            <div className={styles.text}>
+                <h1 className={styles.title}>{title}</h1>
+                <p className={styles.subtitle}>{subtitle}</p>
+            </div>
             {imageOn && <figure>[画像]</figure>}
         </div>
     )
