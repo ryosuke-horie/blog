81c4d0ef4baaf517c3333213464a2f3a0c771579
ryosuke-horie <ryosuke03140@gmial.com>
2023-05-03 18:24:54 +0900

    共通スタイルを利用してフッターのレイアウトを調整

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,13 @@
 import Logo from "components/logo"
+import styles from "styles/footer.module.css"
 
 export default function Footer() {
     return (
-        <footer>
-            <Logo />
+        <footer className={styles.wrapper}>
+            <div className={styles.flexContainer}>
+                <Logo />
+                [ソーシャル]
+            </div>
         </footer>
     )
 }
