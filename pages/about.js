61e92fa7eb80b74acaddfe28836f673fb86babcd
ryosuke-horie <ryosuke03140@gmial.com>
2023-05-03 21:39:13 +0900

    contactコンポーネントを作成

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,7 @@
 import Hero from "components/hero"
 import Container from "components/container"
 import PostBody from "components/post-body"
+import Contact from "components/contact"
 
 export default function About() {
   return (
@@ -23,6 +24,9 @@ export default function About() {
           今までと違うものを作ることで愛着が湧いてきます。そこで興味を持ったことは小さなことでもいいから取り入れて、良いものを作れるようにしています。小さなヒントから新しいものを生み出すようなモノづくりは、これからも続けていきたいです。
         </p>
       </PostBody>
+
+      <Contact />
+
     </Container>
   )
 }
