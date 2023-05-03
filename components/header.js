f2cf61f7dcd0b7cce35848996364f649f748b0a6
ryosuke-horie <ryosuke03140@gmial.com>
2023-05-03 18:18:43 +0900

    共通スタイルを利用してヘッダーのレイアウトを整える

--- components/header.js
+++ components/header.js
@@ -1,11 +1,14 @@
 import Logo from "components/logo"
 import Nav from "components/nav"
+import styles from "styles/header.module.css"
 
 export default function Header() {
     return (
-    <header>
-        <Logo boxOn />
-        <Nav />
-    </header>
+        <header>
+            <div className={styles.flexContainer}>
+                <Logo boxOn />
+                <Nav />
+            </div>
+        </header>
     )
 }
