6ef04e318503c0bcd7fb66b4d66cf28be2908d88
ryosuke-horie <ryosuke03140@gmial.com>
2023-05-03 16:30:36 +0900

    Layoutコンポーネントに共通部分をまとめ、全ページに適用

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,7 @@
-import Header from "components/header"
 import Hero from "components/hero"
-import Footer from "components/footer"
-
 
 export default function Home() {
   return (
-    <>
-      <Header />
-
-      <main>
-        <Hero />
-      </main>
-
-      <Footer />
-    </>
+    <Hero />
   )
 }
