39801e6e2f2ba81425dddb21a7933e6c27b5dee8
ryosuke-horie <ryosuke03140@gmial.com>
2023-05-03 16:23:55 +0900

    importを絶対パスで記述できるようにする

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Header from "../components/header"
-import Hero from "../components/hero"
-import Footer from "../components/footer"
+import Header from "components/header"
+import Hero from "components/hero"
+import Footer from "components/footer"
 
 
 export default function Home() {
