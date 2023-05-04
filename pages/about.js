1fc222c7f070a227b515e96dd496d69d7e6e98d3
ryosuke-horie <ryosuke03140@gmial.com>
2023-05-05 06:50:10 +0900

    タイトル以外のメタを設定

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,13 @@ import eyecatch from 'images/about.jpg'
 export default function About() {
   return (
     <Container>
-      <Meta pageTitle="About" />
+      <Meta 
+        pageTitle="About"
+        pageDesc="About Development acticities"
+        pageImg={eyecatch.src}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
       <Hero title="About" subtitle="About development activities" />
 
       <figure>
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -5,7 +5,7 @@ import Container from "components/container"
 export default function Blog() {
   return (
     <Container>
-      <Meta pageTitle="Blog" />
+      <Meta pageTitle="Blog" pageDesc="ブログの記事一覧" />
       <Hero title="Blog" subtitle="Recent Posts" />
     </Container>
   )
