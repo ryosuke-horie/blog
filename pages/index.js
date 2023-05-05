8b121a28956551c1b3aae6300e9b57ac368e1bcb
ryosuke-horie <ryosuke03140@gmial.com>
2023-05-05 16:02:11 +0900

    TOPページに４件の記事を追加

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,39 @@
+import { getAllPosts } from "lib/api"
 import Meta from "components/meta"
 import Hero from "components/hero"
 import Container from "components/container"
+import Posts from "components/posts"
+import Pagination from "components/pagination"
+import { getPlaiceholder } from "plaiceholder"
 
-export default function Home() {
+// ローカルのデフォルトアイキャッチ画像
+import { eyecatchLocal } from "lib/constants"
+
+export default function Home({ posts }) {
   return (
     <Container>
       <Meta />
       <Hero title="CUBE" subtitle="アプトプットしていくサイト" imageOn />
+      <Posts posts={posts} />
+      <Pagination nextUrl="/blog" nextText="More Posts" />
     </Container>
   )
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPosts(4)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: {
+      posts: posts,
+    },
+  }
 }
