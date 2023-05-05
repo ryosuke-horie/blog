5a1a4379d91641629fd891290bc6f9fb8a4d7bc9
ryosuke-horie <ryosuke03140@gmial.com>
2023-05-05 14:39:16 +0900

    すべての記事ページを動的に生成

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,4 +1,4 @@
-import { getPostBySlug } from "lib/api"
+import { getPostBySlug, getAllSlugs } from "lib/api"
 import { extractText } from "lib/extract-text"
 import Meta from "components/meta"
 import Container from "components/container"
@@ -12,7 +12,7 @@ import { getPlaiceholder } from "plaiceholder"
 // ローカルの代替アイキャッチ画像
 import { eyecatchLocal } from "lib/constants"
 
-export default function Schedule({
+export default function Post({
     title,
     publish,
     content,
@@ -62,8 +62,17 @@ export default function Schedule({
     )
 }
 
-export async function getStaticProps() {
-    const slug = 'micro'
+export async function getStaticPaths() {
+    const allSlugs = await getAllSlugs()
+
+    return {
+        paths: allSlugs.map(({ slug }) => `/blog/${slug}`),
+        fallback: false,
+    }
+}
+
+export async function getStaticProps(context) {
+    const slug = context.params.slug
 
     const post = await getPostBySlug(slug)
 
