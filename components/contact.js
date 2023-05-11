402928cbd36a5850450bde10ae6ea5253ce73d63
ryosuke-horie <ryosuke03140@gmial.com>
2023-05-11 21:37:55 +0900

    addressを削除

--- components/contact.js
+++ components/contact.js
@@ -6,7 +6,6 @@ export default function Contact() {
         <div className={styles.stack}>
             <h3 className={styles.heading}>Contact</h3>
             <Social iconSize="30px" />
-            <address>cube@web.mail.address</address>
         </div>
     )
 }
