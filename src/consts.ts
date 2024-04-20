4ab100f6e9b54c59c774618a17e734ab87c7cf2f
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-04-21 02:03:04 +0900

    エスケープ

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Ryosuke Horie';
-export const SITE_DESCRIPTION = 'Ryosuke Horie`s Parsonal Website and Blog. software engineer.';
+export const SITE_DESCRIPTION = 'Ryosuke Horie\'s Personal Website and Blog. Software Engineer.';
